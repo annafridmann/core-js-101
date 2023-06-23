01c14ce05e509da44179be6ce1dcb38973b57786
github-username <annafridmann@gmail.com>
2023-06-23 16:29:29 +0300

    fix: tasks arrays, func-n-cl functions

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -578,8 +578,14 @@ function selectMany(/* arr, childrenSelector */) {
  *   ['one','two','three'], [2]       => 'three'  (arr[2])
  *   [[[ 1, 2, 3]]], [ 0, 0, 1 ]      => 2        (arr[0][0][1])
  */
-function getElementByIndexes(/* arr, indexes */) {
-  throw new Error('Not implemented');
+function getElementByIndexes(arr, indexes) {
+  // del first elem from array
+  const index = indexes.shift();
+  // rec function
+  if (indexes.length) {
+    return getElementByIndexes(arr[index], indexes);
+  }
+  return arr[index];
 }
 
 
--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -171,8 +171,13 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  // throw new Error('Not implemented');
+  let generator = startFrom - 1;
+  return () => {
+    generator += 1;
+    return (generator);
+  };
 }
 
 
